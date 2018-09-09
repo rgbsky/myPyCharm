3af3157e566db9eece46a642e0933476b4d7687c
rgbsky <839970@qq.com>
2018-09-09 15:30:47 +0800

    first commit

---
+++ first.py
@@ -0,0 +1,20 @@
+import math
+
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+
+#todo: aabbccdd
+
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
